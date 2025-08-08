a04446e1f532e3630a6660a720b357f1f1ee6342
Mohammed C K Munais <marhaba.munees@gmail.com>
2025-08-08 20:58:01 +0530

    changed build path

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserAnimationsModule),
+    ...(appConfig.providers || [])
+  ]
+})
+.catch((err) => console.error(err));
