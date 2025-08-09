09f58377c50bfad0e39ed43ccd4e1c55cf4c0a08
Mohammed C K Munais <marhaba.munees@gmail.com>
2025-08-09 23:31:11 +0530

    changed home page style and started to change content

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink, CommonModule,RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   standalone: true
