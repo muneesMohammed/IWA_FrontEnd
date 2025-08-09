72172142878ed26df49e447ff07e874a9e6a96a2
Muhammed.c <irshadmuhammedch8@gmail.com>
2025-08-09 20:27:30 +0530

    volunteer registration

--- src/app/components/volunteer/volunteer.component.ts
+++ src/app/components/volunteer/volunteer.component.ts
@@ -1,11 +1,51 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-volunteer',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './volunteer.component.html',
   styleUrl: './volunteer.component.css'
 })
 export class VolunteerComponent {
+  volunteerForm!: FormGroup;
 
+  constructor(private fb: FormBuilder, private apiService: ApiService) {
+    // Initialize the form group here if needed
+  }
+  ngOnInit() {
+    this.initform();
+  }
+
+  initform() {
+    this.volunteerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.email]],
+      phone: ['', Validators.required],
+      dob: ['', Validators.required],
+      address: ['', Validators.required],
+      district: ['', Validators.required],
+      state: ['', Validators.required],
+      education: [''],
+      job: ['']
+    });
+  }
+
+  onSubmit() {
+    if (this.volunteerForm.valid) {
+      console.log('Form Submitted', this.volunteerForm.value);
+      this.apiService.addVolunteer(this.volunteerForm.value).subscribe({
+        next: (response) => {
+          console.log('Volunteer registered successfully', response);
+          // Optionally reset the form or show a success message
+          this.volunteerForm.reset();
+        },
+        error: (error) => {
+          console.error('Error registering volunteer', error);
+          // Optionally show an error message to the user
+        }
+      })
+    }
+  }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+
+  }
+  baseUrl = 'https://uym3zhq2j2226x335o2lrcf6wm0qrdce.lambda-url.ap-south-1.on.aws/'
+
+  getVolunteers():Observable<any> {
+    return this.http.get(`${this.baseUrl}volunteer/get-all-volunteers`);
+  }
+
+  addVolunteer(data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}volunteer/create-volunteer`, data);
+  }
+  
 }
